7ee73b0ccbb0fdf9e875e350311909a5e73f403c
raghav161 <raghavbansal242003@gmail.com>
2024-06-29 20:55:37 +0530

    Changed Title

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Zoom_project",
+  title: "Meetsphere",
   description: "Video calling App",
   icons: {
     icon: "/icons/logo.svg",
