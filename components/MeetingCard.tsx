aa9a7c0cbe7e7816c4cbef097815ea8695736d70
raghav161 <raghavbansal242003@gmail.com>
2024-06-29 20:01:38 +0530

    corrected errors

--- components/MeetingCard.tsx
+++ components/MeetingCard.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import Image from "next/image";
 
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
-import { avatarImages } from "@/constants";
 import { useToast } from "./ui/use-toast";
 
 interface MeetingCardProps {
